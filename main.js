e91ba556837a0d0b16e8e8334f35a026f2eb910a
unknown <yonaycl@gmail.com>
2018-03-09 16:23:40 +0000

    paint top ten

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 $.get("https://jsonplaceholder.typicode.com/posts")
     .then((callback) => {
-        console.log(callback);
+        console.log(callback.slice(0,10));
+        paintCards(callback.slice(0,10))
+        
     }).catch((errorCallback) => {
         console.log(errorCallback);
     })
+
+    function paintCards(callbacks){
+        callbacks.forEach((x)=>{
+            $( "body" ).append( `<div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt=${x.title}>
+        <div class="card-body">
+            <p class="card-text">${x.body}</p>
+        </div>
+    </div>`); 
+        })
+        
+    }
